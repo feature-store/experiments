3a0e3372d21fad3ee6272fbe50da6138420e8f35
Sarah Wooders <sarahwooders@gmail.com>
2021-10-09 15:14:28 -0700

    [WIP] STL parameter file evaluation  (#3)
    * anyof
    
    * fix
    
    * add aws confg
    
    * revert
    
    * add initial key policies
    
    * add key selection policy options
    
    * only return key
    
    * start implementing time-specific weights
    
    * plan-level simulation
    
    * add policy evaluation script
    
    Co-authored-by: simon-mo <simon.mo@hey.com>

--- stl/offline/run_2_eval_yahoo_keys.sh
+++ stl/offline/run_2_eval_yahoo_keys.sh
@@ -1,17 +1,14 @@
 set -ex
 
-data_dir="/home/ubuntu/ydata-labeled-time-series-anomalies-v1_0/A4Benchmark/"
+data_dir="/data/wooders/stl/yahoo"
 
-tmp_script=`mktemp`
-for data in `ls $data_dir/A4Benchmark-TS*`
+for data in `ls $data_dir/A4/*`
 do
     key=`basename $data`
     for slide in 6 12 18 24 48 96 168 192 336 672
     do
-        echo python evaluation.py --offline-yahoo-csv-path $data \
+        python evaluation.py --offline-yahoo-csv-path $data \
             --offline-plan-path ./result/offline_1_slide/plan/slide_${slide}_plan.json \
-            --output-path ./result/offline_1_slide/plan_eval/slide_${slide}_key_${key} >> $tmp_script
+            --output-path ./result/offline_1_slide/plan_eval/slide_${slide}_key_${key}
     done
 done
-
-cat $tmp_script | parallel --bar bash -l -c
