5e0263e83145a1ff0b651a41a21144dca44f2ef3
Sarah Wooders <sarahwooders@gmail.com>
2021-10-19 12:43:47 -0700

    reorganize wiki dir

--- wikipedia/benchmark_bert.py
+++
@@ -1,7 +0,0 @@
-from transformers import PyTorchBenchmark, PyTorchBenchmarkArguments
-from pprint import pprint
-
-args = PyTorchBenchmarkArguments(models=["bert-base-uncased"], batch_sizes=[1], sequence_lengths=[100], no_multi_process=True)
-benchmark = PyTorchBenchmark(args)
-results = benchmark.run()
-pprint(results)
--- wikipedia/run_0_generate_data.sh
+++
@@ -1,1 +0,0 @@
-python preprocessing/wiki_api_data.py --run_generate_simulation_data --run_get_questions
