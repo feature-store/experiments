aac5df96414f208f5c21cf106265b76dd7a964ca
Sarah Wooders <sarahwooders@gmail.com>
2022-03-17 15:46:07 -0700

    STL real system  (#18)

--- stl_experimets.sh
+++ stl_experimets.sh
@@ -1,10 +1,10 @@
 export NUMEXPR_MAX_THREADS=128
 
-for slide in 12 672 384 192 96 48 24 
+for slide in 672 384 192 96 48 24 12
 do
-    for workers in 8 6 4 2 1
+    for workers in 1 8 6 4 2 
     do 
-        for policy in "lifo" "fifo"
+        for policy in "fifo" "lifo" 
         do
             TMPDIR=/data/wooders/tmp python workloads/stl/stl_server.py \
                 --experiment stl-yahoo-A4-keys-100-interval-10000-events-200000-queries-200000 \
