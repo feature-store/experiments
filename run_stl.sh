c39246fdf841fb88e7f07c0a63a8e8e7b9a75dc7
Sarah Wooders <sarahwooders@gmail.com>
2022-04-12 00:07:10 +0000

    some modifications to the scheduler

--- run_stl.sh
+++ run_stl.sh
@@ -6,5 +6,17 @@ python workloads/stl/stl_server.py \
  --slide_size=288 \
  --workers=16 \
  --azure_database /home/ubuntu/cleaned_sqlite_3_days_min_ts.db \
- --num_keys=5000 \
- --source_sleep_per_batch 0.001
+ --num_keys=20 \
+ --epsilon 0.1 \
+ --source_sleep_per_batch 0.01
+
+
+python workloads/stl/stl_server.py \
+ --scheduler=ce \
+ --window_size=864 \
+ --slide_size=288 \
+ --workers=16 \
+ --azure_database /home/ubuntu/cleaned_sqlite_3_days_min_ts.db \
+ --num_keys=20 \
+ --epsilon 100 \
+ --source_sleep_per_batch 0.01
